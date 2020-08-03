f76f0e3b6e665db6d5c81311d468945ff0d1d130
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2020-08-02 20:11:30 -0400

    Add a short blurb about me and add a picture of my pretty face 😊

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     // Will be set on the <html /> tag
     siteLanguage: `en`,
     // Used for og:image and must be placed inside the `static` folder
-    siteImage: `/banner.jpg`,
+    siteImage: `/me.jpg`,
     // Twitter Handle
     author: ``,
   },
